124f344b2a310da448e12875d5f50e24cd512a5c
Wahid Tausif Islam <149605488+wahidtausifislam@users.noreply.github.com>
2024-03-31 00:57:17 +0600

    Update The Design

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
